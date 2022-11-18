89700b53e4c49bf621500f104ec369cf2134917b
Alexandr <jwchaganav@gmail.com>
2022-11-18 11:14:56 +0400

    округлили до двух

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int rows = ReadArray("Введите число строк:");
+int columns = ReadArray("Введите число колонок:");
+double[,] numbers = new double[rows,columns];
+
+FillRandomArray(numbers);
+PrintArray(numbers);
+
+// Функции
+void FillRandomArray(double[,] array)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round(rnd.NextDouble(),2);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+        Console.WriteLine();
+    }
+}
+
+int ReadArray(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
